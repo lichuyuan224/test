2c413bfdcb0203aa49c7c7641a64ed058ce553a2
gaojun <jun.gao@cloudchef.io>
2022-09-24 18:05:45 +0800

    大屏全屏

--- src/views/big-screen.vue
+++ src/views/big-screen.vue
@@ -1,6 +1,7 @@
 <template>
   <div class='big-screen' style='width: 100%;'>
-    <img style='width: inherit' :src='imgUrl'>
+    <a-icon type="fullscreen" @click='changeFull'/>
+    <img id='big-screen' style='width: inherit' :src='imgUrl'>
   </div>
 </template>
 <script>
@@ -20,7 +21,37 @@ export default {
       }
       this.imgUrl = routerPathMap[this.$route.path]
   },
-  methods: {}
+  methods: {
+    enable: function(element) {
+      if(element.requestFullScreen) {
+        element.requestFullScreen();
+      } else if(element.mozRequestFullScreen) {
+        element.mozRequestFullScreen();
+      } else if(element.webkitRequestFullscreen) {
+        element.webkitRequestFullscreen();
+      } else if (element.msRequestFullscreen) {
+        element.msRequestFullscreen();
+      }
+    },
+    cancel: function() {
+      if(document.cancelFullScreen) {
+        document.cancelFullScreen();
+      } else if(document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+      } else if(document.webkitExitFullscreen) {
+        document.webkitExitFullscreen();
+      } else if (document.msExitFullscreen) {
+        document.msExitFullscreen();
+      }
+    },
+    changeFull() {
+      if (!this.isFull) {
+        this.enable(document.getElementById('big-screen'))
+      } else {
+        this.cancel()
+      }
+    }
+  }
 }
 </script>
 <style scoped lang='less'></style>
